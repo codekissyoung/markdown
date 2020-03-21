1b5867227d9edee461e896b7fa723ec6351ef331
codekissyoung <cky951010@163.com>
2020-03-21 22:08:03 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-redis-cli eval "$(cat redis.lua)" 2 \
-PROMETHEUS_:histogram:run_cost_name \
-PROMETHEUS_histogram_METRIC_KEYS \
-'{"b":"sum","labelValues":["link"]}' \
-'{"b":"50","labelValues":["link"]}' \
-45 \
-'{"name":"run_cost","help":"","type":"histogram","labelNames":["name"],"buckets":[20,10,30,40,50,60,70]}'
-
